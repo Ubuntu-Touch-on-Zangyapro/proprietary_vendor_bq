d88d8ba1d28973f37b20af7d4d06064173228b67
Danct12 <danct12@disroot.org>
2020-04-27 19:22:49 +0700

    no need

--- sdm660-common/Android.mk
+++ sdm660-common/Android.mk
@@ -43,17 +43,6 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := embms
-LOCAL_MODULE_OWNER := bq
-LOCAL_SRC_FILES := proprietary/app/embms/embms.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := uceShimService
 LOCAL_MODULE_OWNER := bq
 LOCAL_SRC_FILES := proprietary/app/uceShimService/uceShimService.apk
